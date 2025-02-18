011db9a27d1bce47e53761b104e33e668461a860
İsa Öztürk <isa.ozturk@csb.gov.tr>
2025-02-18 18:22:29 +0300

    header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @Output() toggleSidebar = new EventEmitter<void>();
+
+  constructor() { }
+
+  onToggleSidebar() {
+    this.toggleSidebar.emit();
+  }
+}
